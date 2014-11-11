8f27ace70ea3260296f50eed12d42b91c18cc769
alexk-blackops <Alex@Alexanders-MacBook-Pro.local>
2014-11-11 20:50:29 +0300

    work in progress

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-var events = require('events'),
-    util = require('util'),
-    winston = require('winston'),
+var events   = require('events'),
+    util     = require('util'),
+    winston  = require('winston'),
     stackify = require('stackify-logger');
 
 //
@@ -9,8 +9,6 @@ var events = require('events'),
 // for persisting log messages and metadata to a memory array of messages and sending them to Stackify API.
 //
 var Stackify = winston.transports.Stackify = function () {
-    this.lastSent = Date.now();
-    this.output = [];
     this.flag = true; // flag that indicates if logs are being sent properly. if false then new batches of messages won't be sent.
     this.fail_counter = 0; //number of failed attempts of sending logs
     this.timeout = undefined;
@@ -40,15 +38,15 @@ Stackify.prototype.log = function (level, msg, meta, callback) {
     }
 
     var self = this,
-        output = {level: level, msg: msg, meta: meta};
+        output = {Msg: msg, Level: level.toUpperCase(), EpochMs: Date.now(), meta: meta};
 
-    if (level === 'error' || level === 'debug') {
-        this.output.push(output);
+    if (level === 'error') {
+        stackify.storage.push(stackify.checkError(stackify.storage));
     } else {
-        this.output.push(output);
+        stackify.storage.push(output);
     }
 
-    if (this.flag && this.output.length >= stackify.CONFIG.MSG_LIMIT && stackify.CONFIG.APP_DETAILS) {
+    if (this.flag && stackify.storage.length >= stackify.CONFIG.MSG_LIMIT && stackify.CONFIG.APP_DETAILS) {
         this.sendLogs();
 
         self.emit('logged');
@@ -57,49 +55,49 @@ Stackify.prototype.log = function (level, msg, meta, callback) {
 };
 
 Stackify.prototype.clearLogs = function () {
-    this.errorOutput = [];
-    this.writeOutput = [];
+    stackify.storage = [];
 };
 
-Stackify.prototype.sendLogs = function () {
+Stackify.prototype.sendLogs = function (len) {
     var self = this,
-        arr = this.output,
-        data = arr.slice(0, stackify.CONFIG.MSG_LIMIT),
-        success = function() {
+        arr = stackify.storage,
+        length = len || stackify.CONFIG.MSG_LIMIT,
+        data = arr.slice(0, length),
+        success = function () {
             self.fail_counter = 0;
+            self.flag = false;
 
-            arr = arr.slice(stackify.CONFIG.MSG_LIMIT);
+            arr = arr.slice(length);
 
             if (arr.length >= stackify.CONFIG.MSG_LIMIT) {
-                sendLogs();
+                Stackify.prototype.sendLogs();
             } else {
                 self.flag = true;
-                self.lastSent = Date.now();
 
                 if (self.timeout) {
                     clearTimeout(self.timeout);
                 }
-                
+
                 self.timeout = setTimeout(self.checkLogs.bind(self), stackify.CONFIG.SCAN_TIMER);
             }
         },
-        fail = function(arr) {
+        fail = function () {
             self.flag = false;
             self.fail_counter += 1;
 
-            setTimeout(function() {
-                sendLogs()
+            setTimeout(function () {
+                stackify.postLogs(data, success, fail);
             }, stackify.CONFIG.REQUEST_TIMER);
         };
-        
+
     if (this.fail_counter <= stackify.CONFIG.REQUEST_ATTEMPTS) {
         stackify.postLogs(data, success, fail);
     }
 };
 
 Stackify.prototype.checkLogs = function () {
-    var length = this.output(length);
-    if (Date.now() - this.lastSent > stackify.CONFIG.SCAN_TIMER * 1000 && this.flag) {
-        this.sendLogs(this.output);
+    var length = stackify.storage.length;
+    if (this.flag) {
+        this.sendLogs(length);
     }
 };
